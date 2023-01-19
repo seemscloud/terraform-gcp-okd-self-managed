5f97f9e91ca5358480341953983400092ef24d09
Mateusz Katana <mateusz.katana@lppsa.com>
2023-01-20 00:12:40 +0100

    sadf

--- output.tf
+++ output.tf
@@ -3,7 +3,8 @@ output "bastion-external_addresses" {
 }
 
 output "node_group-dns_a_records" {
-  value = concat(module.node_group-master.dns_a_records,
+  value = concat(
+    module.node_group-master.dns_a_records,
     module.node_group-worker.dns_a_records,
     module.node_group-bootstrap.dns_a_records,
     [google_dns_record_set.load_balancer-dns_a_records.name],
