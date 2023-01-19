33b2850285c27b1f9de2cef966300722ba8d063a
Mateusz Katana <mateusz.katana@lppsa.com>
2023-01-20 00:12:16 +0100

    sadf

--- output.tf
+++ output.tf
@@ -1,61 +1,12 @@
-###################################
-#
-#     Instance (Bastion)
-#
 output "bastion-external_addresses" {
   value = module.bastion.external_address
 }
 
-#output "bastion-internal_addresses" {
-#  value = module.bastion.internal_address
-#}
-
-output "bastion-dns_a_records" {
-  value = module.bastion.dns_a_records
-}
-
-###################################
-#
-#     Load Balancing
-#
-output "load_balancer-dns_a_records" {
-  value = [
-    google_dns_record_set.load_balancer-dns_a_records.name
-  ]
-}
-
-###################################
-#
-#     Node Group (Bootstrap)
-#
-#output "node_group_bootstrap-internal_addresses" {
-#  value = module.node_group-bootstrap.internal_addresses
-#}
-
-output "node_group_bootstrap-dns_a_records" {
-  value = module.node_group-bootstrap.dns_a_records
-}
-
-###################################
-#
-#     Node Group (Master)
-#
-#output "node_group_master-internal_addresses" {
-#  value = module.node_group-master.internal_addresses
-#}
-
-output "node_group_master-dns_a_records" {
-  value = module.node_group-master.dns_a_records
-}
-
-###################################
-#
-#     Node Group (Worker)
-#
-#output "node_group_worker-internal_addresses" {
-#  value = module.node_group-worker.internal_addresses
-#}
-
-output "node_group_worker-dns_a_records" {
-  value = module.node_group-worker.dns_a_records
+output "node_group-dns_a_records" {
+  value = concat(module.node_group-master.dns_a_records,
+    module.node_group-worker.dns_a_records,
+    module.node_group-bootstrap.dns_a_records,
+    [google_dns_record_set.load_balancer-dns_a_records.name],
+    [module.bastion.dns_a_records]
+  )
 }
